9dc3314fa58fda41712ba7001dc13bfcf92098be
ptuon <paulootaviotuon@hotmail.com>
2026-09-08 15:15:23 -0300

    Project created

---
+++ src/main/java/com/tuon/tsells/TsellsApplication.java
@@ -0,0 +1,13 @@
+package com.tuon.tsells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsellsApplication.class, args);
+	}
+
+}
