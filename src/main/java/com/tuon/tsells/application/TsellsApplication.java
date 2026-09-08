a1ccf54b8a2a29a239f73dcc16113f56ade889bb
ptuon <paulootaviotuon@hotmail.com>
2026-09-08 16:39:55 -0300

    User entity and resource

--- src/main/java/com/tuon/tsells/application/TsellsApplication.java
+++ src/main/java/com/tuon/tsells/application/TsellsApplication.java
@@ -1,4 +1,4 @@
-package com.tuon.tsells;
+package com.tuon.tsells.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
