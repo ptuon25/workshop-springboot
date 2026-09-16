5089b35a64393a7d1124a19abc425eda5dd86d33
ptuon <paulootaviotuon@hotmail.com>
2026-09-16 16:08:35 -0300

    Exception handling - findById

--- src/main/java/com/tuon/tsells/services/UserService.java
+++ src/main/java/com/tuon/tsells/services/UserService.java
@@ -2,6 +2,7 @@ package com.tuon.tsells.services;
 
 import com.tuon.tsells.entities.User;
 import com.tuon.tsells.repositories.UserRepository;
+import com.tuon.tsells.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
