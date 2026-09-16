f734c015da999e3f323c23bb51248497d5124498
ptuon <paulootaviotuon@hotmail.com>
2026-09-16 16:40:29 -0300

    Exception handling - update

--- src/main/java/com/tuon/tsells/services/UserService.java
+++ src/main/java/com/tuon/tsells/services/UserService.java
@@ -4,6 +4,7 @@ import com.tuon.tsells.entities.User;
 import com.tuon.tsells.repositories.UserRepository;
 import com.tuon.tsells.services.exceptions.DatabaseException;
 import com.tuon.tsells.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
