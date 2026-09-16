08ae6a9a82acc152caed8fd1e6d5b5243f77474d
ptuon <paulootaviotuon@hotmail.com>
2026-09-16 15:41:28 -0300

    User update

--- src/main/java/com/tuon/tsells/resources/UserResource.java
+++ src/main/java/com/tuon/tsells/resources/UserResource.java
@@ -43,4 +43,10 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        User obj = userService.update(id, user);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
