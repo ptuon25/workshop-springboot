707f55418a4e2e8223adba93a5a65f67a089db16
ptuon <paulootaviotuon@hotmail.com>
2026-09-16 16:35:49 -0300

    Exception handling - delete

--- src/main/java/com/tuon/tsells/services/UserService.java
+++ src/main/java/com/tuon/tsells/services/UserService.java
@@ -2,8 +2,11 @@ package com.tuon.tsells.services;
 
 import com.tuon.tsells.entities.User;
 import com.tuon.tsells.repositories.UserRepository;
+import com.tuon.tsells.services.exceptions.DatabaseException;
 import com.tuon.tsells.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+
+        try{
+            userRepository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
